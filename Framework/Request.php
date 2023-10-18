8ddee0769b613ea23ef85dedacc593b9f8348f50
khitun1 <Khitun2001@mail.ru>
2023-10-18 09:34:59 +0500

    Task2

--- Framework/Request.php
+++ Framework/Request.php
@@ -2,18 +2,36 @@
 
 namespace Framework;
 
+include_once PATH_TO_MODELS.'Route.php';
+
 class Request
 {
     private $get_params;
+    private $type;
+    private $path;
+    private $post_params;
     public function __construct()
     {
+        $this->path = $_GET['path'];
         $this->get_params = $_GET;
-
+        unset($this->get_params['path']);
+        $this->post_params = $_POST;
+        if($_SERVER['REQUEST_METHOD'] === 'POST') $this->type = Route::METHOD_POST;
+        if($_SERVER['REQUEST_METHOD'] === 'GET') $this->type = Route::METHOD_GET;
     }
 
     public function getGetParams(): array
     {
         return $this->get_params;
+    }
+
+    public function getType(): int
+    {
+        return $this->type;
+    }
+    public function getPath()
+    {
+        return $this->path;
     }
 
 }
--- Framework/Router.php
+++ Framework/Router.php
@@ -2,18 +2,58 @@
 
 namespace Framework;
 
+include_once PATH_TO_MODELS.'Route.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/App/Controllers/PageController.php';
+
 class Router
 {
-    private $request;
+    private Request $request;
+    public static array $routes = []; //use to be private
     public function __construct(Request $request)
     {
         $this->request = $request;
 
     }
+    private function getCurrentRoute()
+    {
+        $routes = array_filter(self::$routes,
+            fn(Route $route) => $route->getType() == $this->request->getType() && preg_match($route->getMask(), $this->request->getPath()));
+        if (!$routes) {
+            return null;
+        }
+//        var_dump($routes);
+        usort($routes, fn($route_first, $route_second) => count($this->getParamsForController($route_second)) - count($this->getParamsForController($route_first)));
+//        var_dump( $this->getParamsForController($routes[0]));
+        return $routes[0];
+    }
+    private function getParamsForController(Route $route)
+    {
+        $params = [];
+        preg_match_all($route->getMask(), $this->request->getPath(), $params);
+//        echo '<br>-----<br>';
+//        print_r($params);
+//        echo '<br>-----<br>';
+//        print_r(array_slice($params, 1));
+//        echo '<br>-----<br>';
+        return array_map(fn($p) => $p[0], array_slice($params, 1));
+    }
 
     public function getContent()
     {
-        return $this->request->getGetParams();
+//        echo ('Маршруты:<br>');
+//        var_dump(self::$routes);
+        $exec_route = $this->getCurrentRoute();
+        $controller_name = $exec_route->getControllerClass();
+        $method_name = $exec_route->getControllerMethodName();
+        $controller = new $controller_name();
+        $params_to_controller = $this->getParamsForController($exec_route);
+        return call_user_func_array([$controller, $method_name], [$params_to_controller]);
 
+
+    }
+
+    public static function addRoute($route)
+    {
+        array_push(self::$routes, $route);
     }
 }
