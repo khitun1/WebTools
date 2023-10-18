79805ec6788e39b619b1d618eafd49cb76423fbc
khitun1 <Khitun2001@mail.ru>
2023-10-18 11:37:12 +0500

    Task3

--- index.php
+++ index.php
@@ -1,13 +1,27 @@
 <?php
 
+use Dotenv\Dotenv;
 use Framework\Container;
+use Framework\DbConnection;
 
 define('PATH_TO_MODELS',$_SERVER['DOCUMENT_ROOT'].'/framework/');
 
 include_once PATH_TO_MODELS.'Container.php';
 
+
 if ( file_exists(dirname(__FILE__).'/vendor/autoload.php') ) {
     require_once dirname(__FILE__) . '/vendor/autoload.php';
+}
+
+if (file_exists(".env"))
+{
+    $dotenv = Dotenv::createImmutable(__DIR__);
+    $dotenv->load(); //все параметры окружения помещаются в массив $_ENV
+    echo "Окружение загружено<p>";
+    // var_dump($_ENV);
+}
+else {
+    echo "Ошибка хагрузки ENV<br>";
 }
 
 
